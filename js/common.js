9edfff7299fe65f8640df4b18b60772b78df0104
xufq <xufq0817>
2018-03-14 23:06:13 +0800

    login页面改造 引入iview

---
+++ js/common.js
@@ -0,0 +1,6 @@
+document.write("<script language='javascript' src='/libs/vue/vue.min.js'></script>");
+document.write("<link rel='stylesheet' href='/libs/iview/iview.css'>");
+document.write("<script language='javascript' src='/libs/iview/iview.min.js'></script>");
+document.write("<script language='javascript' src='/libs/axios/axios.min.js'></script>");
+
+var baseUrl  = "/springmvc-login-api";
